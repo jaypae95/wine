069d0fd2783f923132cc25965353f677f708c686
jaypae95 <jaypae95@hbsmith.io>
2019-05-06 21:31:10 +0900

    read data

--- src/run.py
+++ src/run.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import os.path as op
 
 
 def read_data(clr='white'):
     file_name = 'winequality-' + clr + '.csv'
-    with open(file_name, 'r') as dataset:
-        df = pd.read_csv(dataset, names=[x for x in range(0, 10)])
+    file_dir = op.abspath(op.join(op.pardir, 'datasets', file_name))
+
+    with open(file_dir, 'r') as dataset:
+        df = pd.read_csv(dataset)
+
+    return df
 
 
 if __name__ == "__main__":
-    read_data()
+    df = read_data()
