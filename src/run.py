8702e93051074834e2256bfa18dfe810103682cc
jaypae95 <jaypae95@hbsmith.io>
2019-05-06 22:35:22 +0900

    Code Frame

--- src/run.py
+++ src/run.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os.path as op
-
+from src import analysis as an
+from src import prediction as pr
 
 def read_data(clr='white'):
     file_name = 'winequality-' + clr + '.csv'
@@ -12,5 +13,28 @@ def read_data(clr='white'):
     return df
 
 
+def quality_processing(df):
+    high = [7, 8, 9, 10]
+    mid = 6
+    low = [0, 1, 2, 3, 4, 5]
+
+    quality = df['quality']
+
+    quality = quality.replace(high, 'high')
+    quality = quality.replace(mid, 'mid')
+    quality = quality.replace(low, 'low')
+
+    df['quality'] = quality
+
+    return df
+
+
 if __name__ == "__main__":
     df = read_data()
+    df = quality_processing(df)
+
+    # data analysis
+    an.test()
+
+    # data prediction
+    pr.test()
