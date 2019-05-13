097c172c9e3c5d322af0f8274f94ab18bc5b163c
jaypae95 <jaypae95@hbsmith.io>
2019-05-13 12:31:48 +0900

    modify function name

--- src/analysis.py
+++ src/analysis.py
@@ -10,26 +10,26 @@ def data_analysis(df):
     # 1. 만족도가 높은 데이터에서 나타나는 특징 분석
     # 만족도가 high인 것만 뽑아서 각 column들 하나 하나 분석
     # low data에서도 비슷한 결과가 나오는 지 파악해야 하기 때문에 low데이터와도 비교
-    analysis_each_columns(df)
+    analysis_each_column(df)
 
     # 2. 만족도가 높은 데이터에서 column들 간의 관계 분석
     high_quality = df[df['quality'] == 'high']
-    analysis_data_quality(high_quality)
+    analysis_columns_by_correlation(high_quality)
 
     # 3. 전체 만족도와 column들 분석
-    analysis_data_quality(df)
+    analysis_columns_by_correlation(df)
 
     # 4. PCA
 
 
-def analysis_each_columns(df):
+def analysis_each_column(df):
     high_quality = df[df['quality'] == 'high']
     low_quality = df[df['quality'] == 'low']
 
     # your code
 
 
-def analysis_data_quality(df):
+def analysis_columns_by_correlation(df):
     df = df.drop('quality', axis=1)  # quality 열 제거
     # plt.title로 제목 설정 가능(재활용성을 고려하여 설정안함)
     plt.figure(figsize=(12, 12))
